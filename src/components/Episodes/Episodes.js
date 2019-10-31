813edf7470bdfc479e26f0729f24d09742ae86c9
Felipe Mogro <fmogro@eluniverso.com>
2019-10-31 10:04:24 -0500

    Agrego paginacion desde API y utilizo linter para formatear mejor el código en mis dos componentes  Changes to be committed: 	modified:   src/components/Episodes/Episodes.js 	modified:   src/components/Pagination/pagination.js

--- src/components/Episodes/Episodes.js
+++ src/components/Episodes/Episodes.js
@@ -1,15 +1,16 @@
-import React,{Component} from 'react';
-import Pagination from '../Pagination/pagination'
+import React, { Component } from "react";
+import Pagination from "../Pagination/pagination";
 
 export class Episodes extends Component {
-  constructor(){
+  constructor() {
     super();
-  this.state = {
-    hasErrors: false,
-    episodios: [{}],
-    img : 'https://images-na.ssl-images-amazon.com/images/I/51aOMcjlkyL._SY445_.jpg'
-  };
-}
+    this.state = {
+      hasErrors: false,
+      episodios: [{}],
+      img:
+        "https://images-na.ssl-images-amazon.com/images/I/51aOMcjlkyL._SY445_.jpg"
+    };
+  }
   componentDidMount() {
     fetch("https://rickandmortyapi.com/api/episode/")
       .then(res => res.json())
@@ -17,34 +18,39 @@ export class Episodes extends Component {
       .catch(() => this.setState({ hasErrors: true }));
   }
 
-  render(){
-        return (
-
-          <div className="container">
-          <div className="row">
-          {this.state.episodios.map(todo =>
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          {this.state.episodios.map(episodio => (
             <div className="col-md-4 d-flex align-items-stretch">
-              <div className="card text-white bg-dark mb-3" style={{width: '18rem'}}>
+              <div
+                className="card text-white bg-dark mb-3"
+                style={{ width: "18rem" }}
+              >
                 <div className="card-body">
-                  <img src={this.state.img} style={{width:'50%'}} alt={todo.name}></img>
-                  <h5 className="card-title">{todo.name}</h5>
-                  <h5 className="card-text">{todo.air_date}</h5>
-                  <h6 className="card-text">{todo.episode}</h6>
-                  <a href={todo.url} className="btn btn-primary">GO</a>
+                  <img
+                    src={this.state.img}
+                    style={{ width: "50%" }}
+                    alt={episodio.name}
+                  ></img>
+                  <h5 className="card-title">{episodio.name}</h5>
+                  <h5 className="card-text">{episodio.air_date}</h5>
+                  <h6 className="card-text">{episodio.episode}</h6>
+                  <a href={episodio.url} className="btn btn-primary">
+                    GO
+                  </a>
                 </div>
-               </div>
-               
               </div>
-          )}
-          </div>
-          <div className="containter">
-                <Pagination></Pagination>
-          </div>
-
+            </div>
+          ))}
         </div>
-        )
-      }
-    }
-
+        <div className="containter">
+          <Pagination></Pagination>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Episodes;
--- src/components/Pagination/pagination.js
+++ src/components/Pagination/pagination.js
@@ -1,40 +1,59 @@
-import React,{Component} from 'react';
+import React, { Component } from "react";
 
 export class Pagination extends Component {
-  constructor(){
+  constructor() {
     super();
-  this.state = {
-    hasErrors: false,
-    paginacion: [{}],
-  };
-}
+    this.state = {
+      hasErrors: false,
+      paginacion: {
+        previous: "#"
+      }
+    };
+  }
   componentDidMount() {
     fetch("https://rickandmortyapi.com/api/episode/")
       .then(res => res.json())
-      .then(res => this.setState({ episodios: res.info }))
+      .then(res => this.setState({ paginacion: res.info }))
       .catch(() => this.setState({ hasErrors: true }));
   }
 
-  render(){
-        return (
-          <div className="container">
-            <div className="row justify-content-md-center">
-            <nav aria-label="Page navigation example">
-              <ul className="pagination">
-                <li className="page-item"><a className="page-link" href="/">Previous</a></li>
-                <li className="page-item"><a className="page-link" href="/">1</a></li>
-                <li className="page-item"><a className="page-link" href="/">2</a></li>
-                <li className="page-item"><a className="page-link" href="/">3</a></li>
-                <li className="page-item"><a className="page-link" href="/">Next</a></li>
-              </ul>
-            </nav>
-            </div>
-
-          </div>
-
-          )}
-
+  paginationLoop = () => {
+    var rows = [];
+    for (let i = 1; i <= this.state.paginacion.pages; i++) {
+      rows.push(i);
     }
+    return rows;
+  };
 
+  render() {
+    return (
+      <div className="container">
+        <div className="row justify-content-md-center">
+          <nav aria-label="Paginacion">
+            <ul className="pagination">
+              <li className="page-item">
+                <a className="page-link" href={this.state.paginacion.previous}>
+                  Previous
+                </a>
+              </li>
+              {this.paginationLoop().map((item, index) => (
+                <li className="page-item">
+                  <a className="page-link" href="/">
+                    {item}
+                  </a>
+                </li>
+              ))}
+              <li className="page-item">
+                <a className="page-link" href={this.state.paginacion.next}>
+                  Next
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Pagination;
